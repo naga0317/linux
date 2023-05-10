c6463820b7cdf8d60b7ab9355c781e9836de9f02
root <naga@naga.myguest.virtualbox.org>
2023-05-10 14:03:52 +0530

    posix semaphore with threads

--- ipc/semaphore/sem.c
+++ ipc/semaphore/sem.c
@@ -3,18 +3,18 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-sem_t mutex;
+sem_t sem;
 int count=1;
 
 void* even(void* arg)
 {
     while(count<100)
     {
-        sem_wait(&mutex);
+        sem_wait(&sem);
         if(count%2==0)
          printf("%d ", count++);
 
-        sem_post(&mutex);
+        sem_post(&sem);
     }
 }
 
@@ -22,23 +22,24 @@ void* odd(void* arg)
 {
     while(count<100)
     {
-        sem_wait(&mutex);
+        sem_wait(&sem);
         if(count%2!=0)
          printf("%d ", count++);
 	
-        sem_post(&mutex);
+        sem_post(&sem);
     }
 }
 
 int main()
 {
-    sem_init(&mutex, 0, 1);
+    sem_init(&sem, 0, 1);
     pthread_t t1,t2;
     pthread_create(&t1,NULL,odd,NULL);
     sleep(2);
     pthread_create(&t2,NULL,even,NULL);
     pthread_join(t1,NULL);
     pthread_join(t2,NULL);
-    sem_destroy(&mutex);
+    sem_destroy(&sem);
+    printf("\n");
     return 0;
 }
